d4ac9e99da41a9b97c142b94ec1f5d6bb4c9388f
Silas Sefas <silassefas23@gmail.com>
2025-07-03 15:48:11 -0300

    feat(curso): primeiro projeto em padrão MVC

---
+++ Curso_C#/Aula_01_Primeiros_Passos/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Aula_01_Primeiros_Passos;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World! Cheguei no C#");
+        Console.WriteLine("Meu Nome é Silas");
+
+        string nome = "Silas";
+        int idade = 25;
+        double altura = 1.81;
+        bool efetivado = true;
+
+        Console.WriteLine($"Meu nome é {nome}, tenho {idade} anos, minha altura é {altura} e efetivado fui: {efetivado} pela Avanade");
+    }
+}
---
+++ PrimeiroMVC/PrimeiroMVC/Controllers/ProdutosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeiroMVC.Controllers
+{
+    public class ProdutosController : Controller //define o clontoler como um controlador MVC
+    {
+        public IActionResult Index() // o index é o nome padrao para a rota
+        {
+            return View(); //retprma a rota para a view, a rota fica dentro da pasta do controller Views/Produtos/Index.cshtml
+        }
+        // toda view para aparecer na pagina precisa estar dentro do _Layout.cshtml que fica na pasta Views/Shared
+        public IActionResult Clientes() { 
+        
+            return View();
+        }
+    }
+}
